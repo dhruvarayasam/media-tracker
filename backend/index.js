9da0007dce408ebfca596ebd1af7c8fc6d8bae97
Dhruva Rayasam <dhruva.rayasam@gmail.com>
2025-01-10 17:42:53 -0500

    added rudimentary Auth0 authentication

--- backend/index.js
+++ backend/index.js
@@ -10,23 +10,43 @@ const app = express();
 app.use(cors({ credentials: true, origin: process.env.CORS_URL }));
 app.use(express.json());
 
-// CONNECT TO MONGO
 mongoose.connect(process.env.CONNECTION_URI).then(() => {
     console.log("Sucessfully connected to Mongo.")
-
-    // Routes
-    app.get('/', (req, res) => {
-        res.send('Hello, World!');
-    });
-
-    app.get('/api/data', (req, res) => {
-        res.json({ message: 'Here is some data!' });
-    });
-
-    // Server start
-    app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
-    });
 }).catch((error) => {
     console.log(error)
 }) 
+
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET_STR,
+  baseURL: process.env.BASE_AUTH_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// BASE
+app.get('/', (req, res) => {
+    // CHECKS FOR AUTH STATUS --> frontend will check response and serve page accordingly
+    res.send(req.oidc.isAuthenticated() ? JSON.stringify({authenticated: true}) : JSON.stringify({authenticated:false}));
+});
+
+// authenticated URLS
+app.get('/profile', requiresAuth(), (req, res) => {
+    // gather data from mongo
+    res.send(JSON.stringify(req.oidc.user));
+});
+
+
+
+
+// Server start
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
