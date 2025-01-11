289af13671a6b4b4d12c12bcc870bb0db774349f
Dhruva Rayasam <dhruva.rayasam@gmail.com>
2025-01-11 12:46:38 -0500

    check auth status client side? not working

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { environment } from '../environments/environment';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-root',
@@ -14,16 +15,36 @@ export class AppComponent implements OnInit {
   baseURL = environment.BASE_URL;
   isAuthenticated:boolean = false;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private auth: AuthService) {}
 
   ngOnInit() {
     // Capture the 'isAuthenticated' query parameter
-    this.route.queryParams.subscribe(params => {
-      const isAuthenticatedParam = params['isAuthenticated'];
-      this.isAuthenticated = isAuthenticatedParam === 'true'; // Convert to boolean
+    // this.route.queryParams.subscribe(params => {
+    //   const isAuthenticatedParam = params['isAuthenticated'];
+    //   this.isAuthenticated = isAuthenticatedParam === 'true'; // Convert to boolean
       
-      console.log(this.isAuthenticated)
+    //   console.log(this.isAuthenticated)
 
+    // });
+
+    this.auth.handleRedirectCallback().subscribe({
+      next: () => {
+        console.log('Redirect callback handled');
+      },
+      error: (err) => {
+        console.error('Error handling redirect callback:', err);
+      },
     });
+
+    // Subscribe to the isAuthenticated observable
+    this.auth.isAuthenticated$.subscribe((isAuth) => {
+      this.isAuthenticated = isAuth;
+      console.log('Authentication Status:', this.isAuthenticated);
+    });
+
+    // this.auth.isAuthenticated$.subscribe((isAuth) => {
+    //   this.isAuthenticated = isAuth;
+    //   console.log(this.isAuthenticated)
+    // });
   }
 }
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,10 @@
 export const environment = {
     production: false,
-    BASE_URL: 'http://localhost:3000'
+    BASE_URL: 'http://localhost:3000',
+    auth0: {
+      domain: 'dev-iczqza1xd5wktvh5.us.auth0.com',
+      clientId: 'imi9bWBgyt0oYIbgRLOr6YmfwHSA1fMs',
+      redirectUri: 'http://localhost:4200',
+    }
   };
   
