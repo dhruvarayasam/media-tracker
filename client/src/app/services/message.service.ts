baa92d050f3954dd676b5f8c1c9954cbec2a6326
Dhruva Rayasam <dhruva.rayasam@gmail.com>
2025-01-13 17:28:39 -0500

    include bearer token

--- client/src/app/services/message.service.ts
+++ client/src/app/services/message.service.ts
@@ -3,12 +3,14 @@ import { mergeMap, Observable, of } from 'rxjs';
 import { environment as env } from '../../environments/environment';
 import { ApiResponseModel, MessageModel, RequestConfigModel } from '../models';
 import { ExternalApiService } from './external-api.service';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
-  constructor(public externalApiService: ExternalApiService) {}
+  constructor(public externalApiService: ExternalApiService, private auth:AuthService) {}
+  bearer_token = ''
 
   getPublicResource = (): Observable<ApiResponseModel> => {
     const config: RequestConfigModel = {
@@ -32,11 +34,17 @@ export class MessageService {
   };
 
   getProtectedResource = (): Observable<ApiResponseModel> => {
+
+    this.auth.getAccessTokenSilently().subscribe((token) => {
+      this.bearer_token = token
+    })
+
     const config: RequestConfigModel = {
       url: `${env.api.serverUrl}/api/protected`,
       method: 'GET',
       headers: {
         'content-type': 'application/json',
+        'Authorization': `Bearer ${this.bearer_token}`
       },
     };
 
