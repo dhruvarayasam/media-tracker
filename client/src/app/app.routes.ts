81c5043478f04987b05371b0eaec7bfa9e8347ff
Dhruva Rayasam <dhruva.rayasam@gmail.com>
2025-01-16 18:33:15 -0500

    added music, movie components and updated routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { CollectionsComponent } from './collections/collections.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MusicComponent } from './music/music.component';
 
 export const appRoutes: Routes = [
   { path: 'profile', component: ProfileComponent },
   { path: 'collections', component: CollectionsComponent },
+  {path: 'movies', component: MoviesComponent},
+  {path: 'music', component: MusicComponent}
 ];
--- client/src/app/collections/collections.component.ts
+++ client/src/app/collections/collections.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-collections',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './collections.component.html',
   styleUrl: './collections.component.css'
 })
