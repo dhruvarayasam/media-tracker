8f1fbf4418d6c1d8392a4e4a6524ddab45c03d95
Dhruva Rayasam <dhruva.rayasam@gmail.com>
2025-04-23 18:48:16 -0400

    added home component and updated routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ProfileComponent } from './profile/profile.component';
 import { CollectionsComponent } from './collections/collections.component';
 import { MoviesComponent } from './movies/movies.component';
 import { MusicComponent } from './music/music.component';
+import { HomeComponent } from './home/home.component';
 
 export const appRoutes: Routes = [
   { path: 'profile', component: ProfileComponent },
   { path: 'collections', component: CollectionsComponent },
   {path: 'movies', component: MoviesComponent},
-  {path: 'music', component: MusicComponent}
+  {path: 'music', component: MusicComponent},
+  {path: '', component: HomeComponent},
 ];
