6ff0e5f6fe3a9ac5a4bab42f274e8eb2c7c72cfe
Dhruva Rayasam <dhruva.rayasam@gmail.com>
2025-04-24 03:11:08 -0400

    updated environment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -9,6 +9,11 @@ export const environment = {
       clientId: 'imi9bWBgyt0oYIbgRLOr6YmfwHSA1fMs',
       redirectUri: 'https://media-tracker-frontend.vercel.app',
       audience: 'https://dev-iczqza1xd5wktvh5.us.auth0.com/api/v2/'
-    }
+    },
+    spotify_details: {
+      clientId: '4ac9faa941464ffba0c0d182057c994a',
+      redirectUri: 'http://localhost:4200/profile',
+      tokenURL: 'https://accounts.spotify.com/api/token',
+  }
   };
   
